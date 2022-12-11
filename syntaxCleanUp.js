bbe487e4642957434222de448771f7af478063fb
Parvin Sattorova <nanajon66@gmail.com>
2022-12-11 17:34:12 -0500

    Initial commit

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,62 @@
-var foods = {apples: 23, 
+// var foods = {apples: 23, 
+//   grapes: 507, 
+//   eggs: 48}
+
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
+  eggs: 48
+  }
+  console.log(Object.keys(foods));
+  console.log("_______________________________________________________");
+  
 
-var person = { height: "6 ft", weight: "160 lbs" }
+// var person = { height: "6 ft", weight: "160 lbs" }
 
-var post = { imageSrc: "./images/beach.png",
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
+  }
+  console.log(Object.values (person));
+  console.log("_______________________________________________________");
+  
+
+// var post = { imageSrc: "./images/beach.png",
 
 
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
+//    caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
+//         number_likes: 0,
+// comments: [] }
+
+var post = { 
+  
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+  }
+  console.log(post.caption);
+  console.log("_______________________________________________________");
 
 
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
-}
+// var post2 =
+// {
+//       imageSrc: "./images/holiday-party.png",
+//       caption: "What a great holiday party omg",
+//       timestamp: "11:37 PM December 31, 2019",
+//       numberLikes: 13,
+//       comments: []
+// }
+
+var post2 = {
+  
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+  }
+  console.log(post2.timestamp);
+  console.log("_______________________________________________________");
+  
